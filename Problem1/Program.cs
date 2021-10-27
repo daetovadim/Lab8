07090aa495c9bbc85b94d09bac2dce2bd30664ea
Вадим Малутин <malutin.vadim@yandex.ru>
2021-10-27 20:57:51 +0800

    Добавьте файлы проекта.

---
+++ Problem1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            DirectoryInfo parentDir = directory.Parent;
+
+            DirectoryInfo[] dirs = parentDir.GetDirectories();
+            Console.WriteLine($"Название папки: {parentDir.Name}\n");
+            foreach (DirectoryInfo dir in dirs)
+            {
+                Console.WriteLine("\tНазвание подкаталога: "+ dir.Name);
+
+                if (dir.GetDirectories().Length > 0)
+                {
+                    Console.WriteLine("\t\tСодержимые подкаталоги:");
+                    foreach (DirectoryInfo dir1 in dir.GetDirectories())
+                        Console.WriteLine("\t\t\t" + dir1.Name);
+                }
+
+                if (dir.GetFiles().Length > 0)
+                {
+                    Console.WriteLine("\t\tСодержимые файлы:");
+                    foreach (FileInfo file1 in dir.GetFiles())
+                        Console.WriteLine("\t\t\t" + file1.Name);
+                }
+
+                Console.WriteLine();
+            }
+
+            if (parentDir.GetFiles().Length > 0)
+            {
+                Console.WriteLine("\tСодержимые файлы:");
+                foreach (FileInfo file in parentDir.GetFiles())
+                    Console.WriteLine("\t\t" + file.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\New.txt";
+            if (!File.Exists(path))            
+                File.Create(path);
+            
+            StreamWriter sw = new StreamWriter(path, false);
+            
+            Random random = new Random();
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)            
+                array[i] = random.Next(0, 100);
+                        
+            foreach (int num in array)            
+                sw.Write($"{num} ");                
+            
+            sw.Close();
+            
+            StreamReader sr = new StreamReader(path);
+
+            string numbers = sr.ReadLine().Trim();
+            Console.WriteLine($"Массив чисел, считанный из файла: {numbers}");
+            string[] numArray = numbers.Split(' ');
+            int sum = 0;
+
+            foreach (string num in numArray)
+            {
+                int i = int.Parse(num);
+                sum += i;
+            }
+            Console.WriteLine($"Полученная сумма: {sum}");
+            Console.ReadKey();            
+        }
+    }
+}
